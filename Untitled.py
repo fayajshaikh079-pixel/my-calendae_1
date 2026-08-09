102c0dc01c497f961623aa158c0213d47b3a00ed
fayajshaikh079-pixel <fayajshaikh079@gmail.com>
2026-08-09 22:42:03 +0530

    Fix month function call in calendar display

--- Untitled.py
+++ Untitled.py
@@ -2,9 +2,9 @@
 import calendar 
 import streamlit as st 
 year = st.number_input("ENTER YEAR :", value=2026, step=1)
-mont = st.number_input("enter month:",min_value=1, max_value=12, value=8)
+month = st.number_input("enter month:",min_value=1, max_value=12, value=8)
 
-st.text(calendar.mont(int(year),int (mont)))
+st.text(calendar.month(int(year),int (month)))
 
 
 
