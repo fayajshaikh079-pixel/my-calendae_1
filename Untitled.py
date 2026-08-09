d67e0a13c0f27930a8c3574d227ff08ec6bfd75a
fayajshaikh079-pixel <fayajshaikh079@gmail.com>
2026-08-09 21:35:42 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,11 @@
+
+import calendar 
+import stremlit as st 
+year = st.number_input("ENTER YEAR :", value=2026, step=1)
+mont = st.number_input("enter month:",min_value=1, max_value=12, value=8)
+
+st.text(calendar.mont(int(year),int (mont)))
+
+
+
+
