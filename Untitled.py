a204fcbe64788c2c09d919a224e74f23b8a50d85
fayajshaikh079-pixel <fayajshaikh079@gmail.com>
2026-08-09 22:38:33 +0530

    Fix import typo and formatting in Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,6 +1,6 @@
 
 import calendar 
-import stremlit as st 
+import streamlit as st 
 year = st.number_input("ENTER YEAR :", value=2026, step=1)
 mont = st.number_input("enter month:",min_value=1, max_value=12, value=8)
 
